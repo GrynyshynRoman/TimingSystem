f5ad54ecfc43da02b191e0493ea96978dffb47a4
Andrii Mogyla <andrew.d3m0@gmail.com>
2015-10-22 23:18:18 +0300

    Trying to set .gitignore

--- src/main/java/com/d3m0/ts/TimingSystem.java
+++ src/main/java/com/d3m0/ts/TimingSystem.java
@@ -8,5 +8,6 @@ import com.d3m0.ts.gui.MainWindow;
 public class TimingSystem {
     public static void main(String[] args) {
         new MainWindow().open();
+        System.out.println("test");
     }
 }
